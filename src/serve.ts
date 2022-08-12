adab0537fc567a05867bb0aa488be96775c7d5f5
shibamoto <silverbirder@gmail.com>
2022-08-12 13:59:08 +0900

    feat: Support websoket

--- src/serve.ts
+++ src/serve.ts
@@ -1,14 +1,68 @@
 import { createServer } from "@graphql-yoga/node";
 import { readFileSync } from "fs";
 import { resolvers } from "./resolvers/resolvers";
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { execute, ExecutionArgs, subscribe } from "graphql";
 
 const typeDefs = readFileSync("./src/schema/schema.graphql", "utf8");
 
-const server = createServer({
-  schema: {
-    typeDefs,
-    resolvers,
-  },
-});
+async function main() {
+  const yogaApp = createServer({
+    schema: {
+      typeDefs,
+      resolvers,
+    },
+    graphiql: {
+      subscriptionsProtocol: 'WS', 
+    },
+  });
+  
+  const httpServer = await yogaApp.start();
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: yogaApp.getAddressInfo().endpoint,
+  });
+  
+  type EnvelopedExecutionArgs = ExecutionArgs & {
+    rootValue: {
+      execute: typeof execute;
+      subscribe: typeof subscribe;
+    };
+  };
+  
+  useServer(
+    {
+      execute: (args) =>
+        (args as EnvelopedExecutionArgs).rootValue.execute(args),
+      subscribe: (args) =>
+        (args as EnvelopedExecutionArgs).rootValue.subscribe(args),
+      onSubscribe: async (ctx, msg) => {
+        const { schema, execute, subscribe, contextFactory, parse, validate } =
+          yogaApp.getEnveloped(ctx);
 
-server.start();
+        const args: EnvelopedExecutionArgs = {
+          schema,
+          operationName: msg.payload.operationName,
+          document: parse(msg.payload.query),
+          variableValues: msg.payload.variables,
+          contextValue: await contextFactory(),
+          rootValue: {
+            execute,
+            subscribe,
+          },
+        };
+
+        const errors = validate(args.schema, args.document);
+        if (errors.length) return errors;
+        return args;
+      },
+    },
+    wsServer,
+  );
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
