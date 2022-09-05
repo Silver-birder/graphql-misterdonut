ebd7551bb89ad1ab994c7cafc7130d56e992cd54
shibamoto <silverbirder@gmail.com>
2022-09-05 22:20:08 +0900

    collect donuts

--- database/crawler/src/routes.ts
+++ database/crawler/src/routes.ts
@@ -1,25 +1,52 @@
-import { Dataset, createJSDOMRouter } from "crawlee";
+import { Dataset, createJSDOMRouter, Request } from "crawlee";
 
 export const router = createJSDOMRouter();
 
-router.addDefaultHandler(async ({ enqueueLinks, window, log }) => {
+router.addDefaultHandler(async ({ window, log, crawler }) => {
   log.info(`enqueueing new URLs`);
-  const options = window.document.querySelectorAll(
-    'select[name="select3"] option'
-  );
-  options.forEach((option) => {
-    const value = option.getAttribute("value");
-    console.log({ value });
-  });
-  await enqueueLinks({
-    label: "detail",
-  });
+  let urls: { label: string; url: string }[] = [];
+  window.document
+    .querySelectorAll('select[name="select3"] option')
+    .forEach((option) => {
+      if (option.getAttribute("selected") !== null) {
+        return;
+      }
+      const value = option.getAttribute("value");
+      urls.push({
+        label: "menu-history",
+        url: `https://www.misterdonut.jp${value}` || "",
+      });
+    });
+  crawler.addRequests(urls);
 });
 
-// router.addHandler("detail", async ({ request, window, log }) => {
-  //   log.info(`${title}`, { url: request.loadedUrl });
-  //   await Dataset.pushData({
-  //     url: request.loadedUrl,
-  //     title,
-  //   });
-// });
+router.addHandler("menu-history", async ({ window, log }) => {
+  log.info(`${window.document.title}, ${window.document.location.href}`);
+  const createdYear = window.document
+    .querySelector('[src^="/enjoy/zukan/donut/img/tit/k2_year_"]')
+    ?.getAttribute("alt");
+  let donuts: any[] = [];
+  window.document.querySelectorAll("[title1]").forEach((donut) => {
+    const category = donut
+      .closest("table")
+      ?.parentElement?.closest("table")
+      ?.querySelectorAll("td")[0]
+      .querySelector("img")
+      ?.getAttribute("alt");
+    const name = donut.getAttribute("title1");
+    const price = donut.getAttribute("title2");
+    const describe = donut.getAttribute("title3");
+    const img = `https://www.misterdonut.jp/enjoy/zukan/donut/${donut.getAttribute(
+      "href"
+    )}`;
+    donuts.push({
+      createdYear,
+      category,
+      name,
+      price,
+      describe,
+      img,
+    });
+  });
+  await Dataset.pushData(donuts);
+});
